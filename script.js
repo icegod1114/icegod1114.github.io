30c822de5b93fe3270a76b596f5ede4c0cf7a28b
icegod1114 <81365711+icegod1114@users.noreply.github.com>
2023-03-30 18:12:55 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,73 +1,49 @@
-//All images taken from PEXELS.COM - free stock images
-"use strict";
+const submitBtn = document.querySelector('[data-action="submit"]');
+submitBtn.addEventListener("click", processFormData);
 
-window.onload = function() {
+function processFormData(e) {
+  // 方法 1-1：getElementById - 從 input
+  // const nameElement = document.getElementById("name");
+  // const name = nameElement.value;
+  // const emailElement = document.getElementById("email");
+  // const email = emailElement.value;
 
+  // 方法 1-2：getElementById - 從 form
+  // const formElement = document.getElementById("form");
+  // const name = formElement[0].value;
+  // const email = formElement[1].value;
 
-//Navigation bar buttons
-var about = document.querySelector("#navabout");
-var services = document.querySelector("#navServices");
-var contact = document.querySelector("#navDay");
-var write = document.querySelector("#navwrite");
-var read = document.querySelector("#navread");
-var expert = document.querySelector("#navexpert");
-var books = document.querySelector("#navbooks");
+ 
 
-about.onclick = function() {
-  document.querySelector("#aboutRow").scrollIntoView();
-  window.scrollBy(0, -50);
-}
+  // 方法 2：getElementsByTagName
+  
+  // const inputElement = document.getElementsByTagName('input');
+  // const name = inputElement[0].value;
+  // const email = inputElement[1].value;
 
-services.onclick = function() {
-  document.querySelector("#servicesRow").scrollIntoView();
-  window.scrollBy(0, -50);
-}
+  // 方法 3：getElementsByName
+  // const nameElement = document.getElementsByName('name');
+  // const name = nameElement[0].value;
+  // const emailElement = document.getElementsByName('email');
+  // const email = emailElement[0].value;
 
-contact.onclick = function() {
-  document.querySelector("#phoneDay").scrollIntoView();
-  window.scrollBy(0, -50);
-}
+  // 方法 4：取得表單元素
+  //取得 name 屬性為 form 的表單
+  const form = document.forms['form'];
+  //取 elements 集合中 name 屬性為 name 的值
+  const acc1 = form.elements.acc1.value;
+  //取 elements 集合中 name 屬性為 email 的值
+  const pd1 = form.elements.pd1.value;
 
-write.onclick = function() {
-  document.querySelector("#phonewrite").scrollIntoView();
-  window.scrollBy(0, -50);
-}
-
-read.onclick = function() {
-  document.querySelector("#phoneread").scrollIntoView();
-  window.scrollBy(0, -50);
-}
-
-expert.onclick = function() {
-  document.querySelector("#phoneexpert").scrollIntoView();
-  window.scrollBy(0, -50);
-}
-
-books.onclick = function() {
-  document.querySelector("#phonebooks").scrollIntoView();
-  window.scrollBy(0, -50);
-}
-
-//Learn more jumbotron button jump to about div
-var bannerBtn = document.querySelector("#bannerBtn");
-bannerBtn.onclick = function() {
-  document.querySelector("#aboutRow").scrollIntoView();
-  window.scrollBy(0, -50);
-}
-
-//Clear input on click in the form
-var formName = document.querySelector("#formName");
-formName.onclick = function() {
-  formName.value = "";
-}
-
-var formEmail = document.querySelector("#formEmail");
-formEmail.onclick = function() {
-  formEmail.value = "";
-}
-
-var formMessage = document.querySelector("#formMessage");
-formMessage.onclick = function() {
-  formMessage.value = "";
-}
+  if (acc1=='tank'){
+    if (pd1=='1114'){
+        alert('您的數學成績為100分');
+    }
+    else{
+        alert('帳號密碼錯誤');
+    }
+  }
+  else{
+    alert('帳號密碼錯誤');
+  }
 }
